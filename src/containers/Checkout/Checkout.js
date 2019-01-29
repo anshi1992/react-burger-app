0787c7d0e504d9a11b2ad6b8ac1f719669273522
Anshita <anshibits@gmail.com>
2019-01-29 23:26:19 +0530

    contact data added

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import {Route} from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
+
+
 
 class Checkout extends Component{
     state={
@@ -10,12 +14,35 @@ class Checkout extends Component{
             cheese: 0
         }
     }
+
+    componentDidMount(){
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        for(let param of query.entries()){
+            //['salad','1']
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({
+            ingredients: ingredients
+        });
+    }
+
+    checkoutCancelledHandler =()=>{
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = ()=>{
+        this.props.history.replace('/checkout/contact-data');
+    }
     render(){
         return (
             <div>
                 <CheckoutSummary
                     ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler}
                 />
+                <Route path={this.props.match.path +'/contact-data'} component={ContactData}/>
             </div>
         );
     }
