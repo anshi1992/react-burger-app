c67d327c0bffb06a9154d277474d44045ddc3d98
Anshita <anshibits@gmail.com>
2019-01-29 19:38:45 +0530

    burger project
    burger after routing

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,37 @@
+import React      from 'react';
+import Aux        from '../Aux';
+import classes    from './Layout.css';
+import Toolbar    from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends React.Component{
+    state={
+        showSideDrawer: false
+    };
+
+    sideDrawerClosedHandler =() =>{
+        this.setState({showSideDrawer: false});
+    }
+
+    sideDrawerToggleHandler =()=>{
+        this.setState(prevState =>{
+            return {showSideDrawer: !prevState.showSideDrawer};
+        });
+    }
+
+    render(){
+        return (<Aux>
+            <div>
+                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <SideDrawer
+                    open={this.state.showSideDrawer}
+                    closed={this.sideDrawerClosedHandler}/>
+            </div>
+            <main className={classes.Content}>
+                {this.props.children}
+            </main>
+        </Aux>);
+    }
+}
+
+export default Layout;
