3daad09048c9e945c10b44933e11aa157cd9055e
Anshita <anshibits@gmail.com>
2019-02-03 19:47:59 +0530

    input component created

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -3,15 +3,60 @@ import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css'
 import axios from '../../../axios-orders';
 import Spinner from '../../../components/UI/Spinner/Spinner';
-
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends React.Component{
     state = {
-        name: '',
-        email:'',
-        address: {
-            street: '',
-            postalCode:''
+        orderForm :{
+            name: {
+                elementType : 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street:{
+                elementType : 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType : 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP'
+                },
+                value: ''
+            },
+            country: {
+                elementType : 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country'
+                },
+                value: ''
+            },
+            email:{
+                elementType : 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your E-Mail'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType : 'select',
+                elementConfig: {
+                    options: [{value: 'fastest', displayValue: 'Fastest'},
+                    {value: 'cheapest', displayValue: 'Cheapest'}
+                ]
+            },
+                value: ''
+            },
         },
         loading: false
     }
@@ -23,16 +68,7 @@ class ContactData extends React.Component{
         const order = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Anshita',
-                address:{
-                    street:'PB Road',
-                    zipCode: '110060',
-                    country: 'India'
-                },
-                email:'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+            
         };
         axios.post('/orders.json', order)
             .then(response =>{
@@ -49,12 +85,25 @@ class ContactData extends React.Component{
     }
 
     render(){
+        const formElementsArray = [];
+        for(let key in this.state.orderForm){
+            formElementsArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            });
+        }
+
         let form =(
             <form>
-                    <input className={classes.Input} type="text" name="name" placeholder="Your name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your email" />
-                    <input className={classes.Input} type="text" name="street" placeholder="street" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal code" />
+                    
+                    {formElementsArray.map(formEl =>( 
+                    <Input elementType={formEl.config.elementType} 
+                        key={formEl.id}
+                        elementConfig={formEl.config.elementConfig}
+                        value={formEl.config.value}
+                    />)
+                    )}
+
                     <Button btnType={"Success"}
                             clicked={this.orderHandler}
                     >ORDER</Button>
